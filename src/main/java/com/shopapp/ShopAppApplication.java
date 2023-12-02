d51c7e0d4f853e438f4de88de0d75c25fe59ba3e
Dinh Dung <dungdung23092002@gmail.com>
2023-12-02 14:34:47 +0700

    this is first commit

---
+++ src/main/java/com/shopapp/ShopAppApplication.java
@@ -0,0 +1,14 @@
+package com.shopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShopAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopapp/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
